0631c4a2d783f66ffeabd234a137b9d098d83689
Madhurima K <127230970+Cookiiiiiie@users.noreply.github.com>
2023-07-30 00:58:37 +0530

    Update booksummary.py

--- BookSummaryProject/booksummary.py
+++ BookSummaryProject/booksummary.py
@@ -2,11 +2,10 @@ import csv
 import openai
 
 # Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
-openai.api_key = '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'
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
 def generate_summary(book_name, book_content):
     prompt = f"Summarize the book '{book_name}' in brief. The book is about {book_content}."
-    # Rest of the code remains the same
     response = openai.Completion.create(
         engine="text-davinci-002",  # You can use a different engine if you prefer
         prompt=prompt,
@@ -17,7 +16,8 @@ def generate_summary(book_name, book_content):
     return response.choices[0].text.strip()
 
 def main(input_csv_file):
-    with open('c:\\BookSummaryProject\\books.csv', "r") as file:
+    with open('Location_of_books.csv_file', "r") as file:  
+        #Replace 'Location_of_books.csv_file' with actual location of your books.csv file
         reader = csv.reader(file)
         next(reader)  # Skip header row if present
 
@@ -25,9 +25,7 @@ def main(input_csv_file):
             book_name = row[0]
             book_content = row[1]  # Assuming the book content is in the second column
             summary = generate_summary(book_name, book_content)
-            # Rest of the code remains the same
-
-
+            
             # Save summary to a text file
             output_file = f"{book_name}_summary.txt"
             with open(output_file, "w") as summary_file:
